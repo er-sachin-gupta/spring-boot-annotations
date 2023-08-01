01422f0f8aae180844661f4af1545b30118cbb80
Sachin Gupta <sgs.engineer@yahoo.co.in>
2023-08-01 11:56:53 +0530

    Stereotype Annotations - @Controller, @Service and @Repository

---
+++ src/main/java/net/javaguides/springannotations/controller/MyController.java
@@ -0,0 +1,12 @@
+package net.javaguides.springannotations.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String hello () {
+        return "Hello Controller";
+    }
+
+}
---
+++ src/main/java/net/javaguides/springannotations/repository/MyRepository.java
@@ -0,0 +1,12 @@
+package net.javaguides.springannotations.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MyRepository {
+
+    public String hello() {
+       return "Hello Repository";
+    }
+
+}
