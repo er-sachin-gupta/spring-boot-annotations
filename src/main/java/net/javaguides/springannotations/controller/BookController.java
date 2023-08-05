346bccdd50f9264a1d778f9fe6783b415580d118
Sachin Gupta <sgs.engineer@yahoo.co.in>
2023-08-05 09:03:41 +0530

    @RestController Annotation

--- src/main/java/net/javaguides/springannotations/controller/BookController.java
+++ src/main/java/net/javaguides/springannotations/controller/BookController.java
@@ -4,19 +4,22 @@ import net.javaguides.springannotations.beans.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
+//@ResponseBody
+@RestController
 public class BookController {
 
     @RequestMapping("/hello")
-    @ResponseBody
+//    @ResponseBody
     public String helloWorld() {
         return "Radhe Krishna";
     }
 
 
     @RequestMapping("/book")
-    @ResponseBody
+//    @ResponseBody
     public Book getBook() {
         Book book = new Book(1, "Bhagwad Geeta", "How to Live");
         return book;
