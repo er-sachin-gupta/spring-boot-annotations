06135dff776ce963aa9efa3abf01fc73bee3eb74
Sachin Gupta <sgs.engineer@yahoo.co.in>
2023-08-05 09:14:59 +0530

    @RequestMapping Annotation

--- src/main/java/net/javaguides/springannotations/controller/BookController.java
+++ src/main/java/net/javaguides/springannotations/controller/BookController.java
@@ -1,14 +1,19 @@
 package net.javaguides.springannotations.controller;
 
 import net.javaguides.springannotations.beans.Book;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
 
 //@Controller
 //@ResponseBody
 @RestController
+@RequestMapping("/api")
 public class BookController {
 
     @RequestMapping("/hello")
@@ -18,7 +23,12 @@ public class BookController {
     }
 
 
-    @RequestMapping("/book")
+    @RequestMapping(
+            value = {"/book", "/java", "/core-java", "/geeta"},
+            method = RequestMethod.GET,
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
+            consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }
+    )
 //    @ResponseBody
     public Book getBook() {
         Book book = new Book(1, "Bhagwad Geeta", "How to Live");
