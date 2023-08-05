a2141126f302387a47a0aff2e16fe3b4fbddcaa3
Sachin Gupta <sgs.engineer@yahoo.co.in>
2023-08-05 09:32:45 +0530

    @PostMapping Annotation

--- src/main/java/net/javaguides/springannotations/controller/BookController.java
+++ src/main/java/net/javaguides/springannotations/controller/BookController.java
@@ -1,7 +1,9 @@
 package net.javaguides.springannotations.controller;
 
 import net.javaguides.springannotations.beans.Book;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,6 +33,16 @@ public class BookController {
     public Book getBook() {
         Book book = new Book(1, "Bhagwad Geeta", "How to Live");
         return book;
+    }
+
+//    @RequestMapping(value = "/books/create", method = RequestMethod.POST)
+//    @ResponseStatus(value = HttpStatus.CREATED)
+    @PostMapping(value = "/books/create")
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        System.out.println(book.getId());
+        System.out.println(book.getTitle());
+        System.out.println(book.getDescription());
+        return new ResponseEntity<>(book, HttpStatus.CREATED);
     }
 
 }
